ad6a039fa346b7a43acae0b435aed1b5c6f6beea
sindelre <rob@sindelar.com>
2025-04-26 00:12:38 -0500

    Update doggy.js

--- doggy.js
+++ doggy.js
@@ -1,4 +1,4 @@
-aexport default {
+export default {
   async fetch(request) {
     // Call the web service
     await fetch("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=dcebb89225ed4cc399209c94b5e28e49&deviceNames=Pixel%206&text=Stop");
