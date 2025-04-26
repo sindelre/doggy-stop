ec5681fceb7ba23e580a6855506e02544240f3a4
sindelre <rob@sindelar.com>
2025-04-25 23:14:30 -0500

    Update doggy.js

--- doggy.js
+++ doggy.js
@@ -3,10 +3,7 @@ export default {
     // Call the web service
     await fetch("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=dcebb89225ed4cc399209c94b5e28e49&deviceNames=Pixel%206&text=Stop");
 
-    // Wait for 10 seconds
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
     // Redirect the user
-    return Response.redirect("https://doggy.wizman.net/cam", 302);
+    return Response.body("Recording Stopped",200);
   }
 };
