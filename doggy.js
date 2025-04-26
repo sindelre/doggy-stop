97183809590e2d11c63e86cb0acd6132e3f0d1d1
sindelre <rob@sindelar.com>
2025-04-26 00:04:50 -0500

    Update doggy.js

--- doggy.js
+++ doggy.js
@@ -1,10 +1,10 @@
-export default {
+aexport default {
   async fetch(request) {
     // Call the web service
     await fetch("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=dcebb89225ed4cc399209c94b5e28e49&deviceNames=Pixel%206&text=Stop");
 
     // Redirect the user
-    return new Response("Recording Stopped", {
+    return new Response("Camera Stopped", {
       status: 200,
       headers: {
         "Content-Type": "text/plain",
