f71450d8964d5b3924157f3e80720c64607f0609
sindelre <rob@sindelar.com>
2025-04-25 23:25:52 -0500

    Update doggy.js

--- doggy.js
+++ doggy.js
@@ -4,11 +4,11 @@ export default {
     await fetch("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=dcebb89225ed4cc399209c94b5e28e49&deviceNames=Pixel%206&text=Stop");
 
     // Redirect the user
-    return Response("Recording Stopped", {
+    return new Response("Recording Stopped", {
       status: 200,
       headers: {
         "Content-Type": "text/plain",
       },
     });
-  }
+  },
 };
