1aa6c8594d734011bc0a224f5a5fbbc6da5f6c74
sindelre <rob@sindelar.com>
2025-04-25 23:05:03 -0500

    Update doggy.js

--- doggy.js
+++ doggy.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     // Call the web service
-    await fetch("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=dcebb89225ed4cc399209c94b5e28e49&deviceNames=Pixel%206&text=Record");
+    await fetch("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=dcebb89225ed4cc399209c94b5e28e49&deviceNames=Pixel%206&text=Stop");
 
     // Wait for 10 seconds
     await new Promise(resolve => setTimeout(resolve, 1000));
